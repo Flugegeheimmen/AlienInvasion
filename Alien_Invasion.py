f7a44bd352d482b47791178520a881d82a6f7246
Flugegeheimmen <vseprodam7@gmail.com>
2022-02-08 09:59:25 +0300

    Second commit

--- Alien_Invasion.py
+++ Alien_Invasion.py
@@ -5,6 +5,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 
 class AlienInvasion:
@@ -21,6 +22,10 @@ class AlienInvasion:
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
+
 
     def run_game(self):
         """Запуск основного цикла игры"""
@@ -41,9 +46,6 @@ class AlienInvasion:
                 self.bullets.remove(bullet)
         # print(len(self.bullets))
         self._update_screen()
-
-
-
 
 
     def _check_events(self):
@@ -81,6 +83,11 @@ class AlienInvasion:
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
 
+    def _create_fleet(self):
+        """Создание флота пришельцев"""
+        alien = Alien(self)
+        self.aliens.add(alien)
+
 
     def _update_screen(self):
         """Обновляет изображения на экране и отображает новый экран"""
@@ -88,6 +95,8 @@ class AlienInvasion:
         self.ship.blitme()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+        self.aliens.draw(self.screen)
+
         pygame.display.flip()
 
 
--- settings.py
+++ settings.py
@@ -3,14 +3,14 @@ class Settings():
     def __init__(self):
         self.screen_width = 1200
         self.screen_height = 600
-        self.bg_color = (205, 205, 205)
+        self.bg_color = (255, 255, 255)
         self.bullet_allowed = 3
 
         #Ship Settings
         self.ship_speed = 1.5
 
         #Параметры Снаряда
-        self.bullet_speed = 1
+        self.bullet_speed = 3
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
