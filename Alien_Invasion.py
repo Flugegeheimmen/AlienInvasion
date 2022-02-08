de2abcf5cdf32c4032ecddaec2db6b49328ef7fc
Flugegeheimmen <vseprodam7@gmail.com>
2022-02-08 10:12:55 +0300

    Second commit

--- Alien_Invasion.py
+++ Alien_Invasion.py
@@ -85,7 +85,22 @@ class AlienInvasion:
 
     def _create_fleet(self):
         """Создание флота пришельцев"""
+        #Далее мы создаем интервал между соседними пришельцами который равен ширине пришельца
         alien = Alien(self)
+        alien_width = alien.rect.width
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
+
+        #Создание первого ряда пришельцев
+
+        for alien_number in range(number_aliens_x):
+            self._create_alien(alien_number)
+
+    def _create_alien(self, alien_number):
+        alien = Alien(self)
+        alien_width = alien.rect.width
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
         self.aliens.add(alien)
 
 
