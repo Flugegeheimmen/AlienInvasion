8cce37b46b605200ab4480d5dbc4d34e93921156
Flugegeheimmen <vseprodam7@gmail.com>
2022-02-08 11:13:58 +0300

    Second commit

--- Alien_Invasion.py
+++ Alien_Invasion.py
@@ -46,11 +46,11 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
-        # print(len(self.bullets))
-        self._update_screen()
+
 
     def _update_aliens(self):
         """Обновление позиции всех пришельцев во флоте"""
+        self._check_fleet_edges()
         self.aliens.update()
 
     def _check_events(self):
@@ -114,6 +114,18 @@ class AlienInvasion:
         alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
 
+    def _check_fleet_edges(self):
+        """Реагирует на достижение пришельцем края экрана"""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """Опускает весь флот и меняет направление"""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+            self.settings.fleet_direction *= -1
 
     def _update_screen(self):
         """Обновляет изображения на экране и отображает новый экран"""
